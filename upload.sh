d9d8ca574406636792c460c68cc280d96fd146d9
AndyLiu <andyliu.92@outlook.com>
2022-04-10 14:02:46 +0800

    FINISHED: pa2-1 exp report

--- upload.sh
+++ upload.sh
@@ -6,6 +6,10 @@ cd PA/ICS_PA
 git pull origin master
 
 echo "**********************************************************"
+echo "sync to github"
+git push github master
+
+echo "**********************************************************"
 echo "git commit on CS server"
 ssh -p 5227 pa201180078@114.212.10.200 "cd /home/pa201180078/pa_nju ; git add . ; git commit -m 'before sync from comfluter.life'"
 
